da74aaefa57ca4c888d24bc4f2714c8577df3b73
Sami Badawi <sami@citymaps.com>
2017-07-23 18:04:19 -0400

    Added webgl-helper.ts it works with test webpage

---
+++ src/webgl-helper.ts
@@ -0,0 +1,80 @@
+/**
+ * 
+ * @param glContext get from canvas
+ * @param shaderType either 'vertex' | 'fragment'
+ * @param source 
+ */
+export function compileShader(
+    glContext: WebGLRenderingContext,
+    shaderType: 'vertex' | 'fragment',
+    source: string): WebGLShader {
+    const shader = glContext.createShader(shaderType === 'vertex' ? glContext.VERTEX_SHADER : glContext.FRAGMENT_SHADER)
+    glContext.shaderSource(shader, source)
+    glContext.compileShader(shader)
+    if (!glContext.getShaderParameter(shader, glContext.COMPILE_STATUS)) {
+        console.log("could not compile " + shaderType + " shader:\n\n" + glContext.getShaderInfoLog(shader))
+        glContext.deleteShader(shader)
+        return null
+    } else {
+        return shader
+    }
+}
+
+/**
+ * 
+ * @param glContext get from canvas
+ * @param fragmentShaderSource source as string
+ * @param vertexShaderSource source as string
+ */
+export function linkWebGLprog(
+    glContext: WebGLRenderingContext,
+    fragmentShaderSource: string,
+    vertexShaderSource: string) {
+
+    if (!glContext)
+        console.log('WebGL is not supported for this device')
+    else if (typeof fragmentShaderSource !== 'string')
+        console.log('Fragment shader was not defined')
+    else if (typeof vertexShaderSource !== 'string')
+        console.log('Vertex shader was not defined')
+    else {
+        const compiled_vs = compileShader(glContext, 'vertex', vertexShaderSource)
+        const compiled_fs = compileShader(glContext, 'fragment', fragmentShaderSource)
+
+        if (!compiled_fs || !compiled_vs) return null
+
+        const program = glContext.createProgram()
+        glContext.attachShader(program, compiled_vs)
+        glContext.attachShader(program, compiled_fs)
+        glContext.linkProgram(program)
+
+        if (!glContext.getProgramParameter(program, glContext.LINK_STATUS)) {
+            console.log('could not link the shader program!')
+            glContext.deleteProgram(program)
+            glContext.deleteProgram(compiled_vs)
+            glContext.deleteProgram(compiled_fs)
+            return null
+        } else {
+            glContext.useProgram(program)
+            return glContext
+        }
+    }
+    return null
+}
+
+export function clearCanvas(
+    canvas: HTMLCanvasElement,
+    r: number,
+    g: number,
+    b: number,
+    alpha: number): void {
+    const context: WebGLRenderingContext = canvas.getContext("webgl")
+
+    // Only continue if WebGL is available and working
+    if (!context) {
+        console.log('WebGL not supported');
+        return;
+    }
+    context.clearColor(r, g, b, alpha);
+    context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
+}
