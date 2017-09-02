b364483ed16d1f47c51ea5ee0dcf0f20aa1eefcd
Sami Badawi <sami@citymaps.com>
2017-09-01 23:43:36 -0400

    There is a button to cycle through different image processing operations

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-import {fourFourty} from './math_helper'
+import { fourFourty } from './math_helper'
+import * as io from './image-operation'
 import * as m from 'mithril'
 import * as _ from 'lodash'
 
 const word1 = "ShapeLogic";
 const word2 = "TypeScript" // _.trim("  TypeScript   ");
 
-const hello = m('h1', [`${word1} ${word2} ${fourFourty}`]);
+const hello = m('h1', [`${word1} ${word2}`]);
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+const colorSequenceArray = [
+    'rgba',
+    'rbga',
+    'bgra'
+]
+
+let colorIndex = 0
+let imageSource = "./img/Lenna.png"
+
+function showImage(): void {
+    colorIndex = (colorIndex + 1) % colorSequenceArray.length;
+    const colorSequence = colorSequenceArray[colorIndex];
+    io.drawImageInContext(canvas, imageSource, colorSequence, 0.5, 0.8, 1)
+}
 
 m.render(document.getElementById("extra"), [hello,
-	m('p', ["An exploration of TypeScript's suitability for computer vision."]),
-	m('p', ["ShapeLogic TypeScript might serve as a front end for ShapeLogic Scala."]),
-	m('p', ["The plan is to keep dependencies minimal, Mithril and Lodash. Both are included in this project"]),
-	m('img[src="img/Lenna.png"]'),
-	m('p', ["TypeScript seems great but it is was hard to get it running without Webpack, Browserify or Gulp"])
+    m('p', ["Computer vision in TypeScript and WebGL"]),
+    m('button', { onclick: showImage }, `Process Image: ${imageSource}`),
+    m('p', ["Will process image"])
 ]
 );
 
